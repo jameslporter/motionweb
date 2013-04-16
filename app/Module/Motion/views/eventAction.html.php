cad9f92711d4f5bce4ed34dd8df3c33d52f5536f
Evan Brown <noggin@gmail.com>
2013-04-16 14:38:14 -0500

    Added carousel to view the images created. This should become a config setting.

--- app/Module/Motion/views/eventAction.html.php
+++ app/Module/Motion/views/eventAction.html.php
@@ -1,3 +1,8 @@
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('.carousel').carousel();
+    });
+</script>
 <?php
 $view->head()->title('Event view from '.$date);
 
@@ -5,10 +10,27 @@ $view->head()->title('Event view from '.$date);
 echo '<li><a href="/event/delete/'.$stamp.'">Delete this event</a></li>
     </ul>';
         
+?>
+    <div id="myCarousel" class="carousel slide">
+
+    <!-- Carousel items -->
+    <div class="carousel-inner">
+<?
+$first = true;
+foreach($frames as $frame){
+	echo '<div class="item '.($first?'active':'').'"><center><img src="'.$frame->filename.'"></center>';
+        echo '<div class="carousel-caption"><p>'.$frame->time_stamp.'</p></div></div>';
+        $first = false;
+}
+?>
+    </div>
+    <!-- Carousel nav -->
+    <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+    <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+    </div>
+    
+<?
 echo '<video width="640" height="480" controls>
   <source src="'.$video.'" type="video/ogg">
 Your browser does not support the video tag.
 </video> <br>';
-foreach($frames as $frame){
-	echo '<img src="'.$frame->filename.'"><br>';
-}
